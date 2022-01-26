6640b45c3584bc3b98075dfa18b62f25c531f420
Diya S <60971672+diaas14@users.noreply.github.com>
2022-01-27 02:26:07 +0530

    27012022 arrays practice

---
+++ 16_3Sum_Closest.java
@@ -0,0 +1,30 @@
+/**
+ * Given an integer array nums of length n and an integer target
+ * find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers.
+ * You may assume that each input would have exactly one solution.
+ */
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int j, k;
+        int closestSum=10000, sum;        
+        for (int i=0; i<nums.length; i++) {
+            if (i!=0 && i < nums.length) 
+                if (nums[i] == nums[i-1]) continue;
+            j=i+1;
+            k=nums.length-1;
+            while (j<k) {
+                if (nums[j]+nums[k] == target-nums[i])
+                    return target;
+                sum = nums[i]+nums[j]+nums[k];
+                closestSum = (Math.abs(target - sum) < Math.abs(target - closestSum)) ? sum : closestSum;
+                if (nums[j]+nums[k] < target-nums[i])
+                    j++;
+                else k--;
+            }
+        }
+        return closestSum;
+    }
+}
