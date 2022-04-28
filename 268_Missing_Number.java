3125ecc042ebb1d2dfa310df98c4e71852fbd73d
Diya S <60971672+diaas14@users.noreply.github.com>
2022-04-28 23:25:06 +0530

    28042022 arrays practice

---
+++ 268_Missing_Number.java
@@ -0,0 +1,12 @@
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n], 
+ * return the only number in the range that is missing from the array.
+ */
+  
+  class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int num: nums) sum += num;
+        return (nums.length * (nums.length + 1)) / 2 - sum;
+    }
+}
