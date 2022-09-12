f3f4873f635500b8c157a099f1d3ac6acce4fe5f
Diya S <60971672+diaas14@users.noreply.github.com>
2022-09-12 15:54:35 +0530

    Update and rename 16_3Sum_Closest.java to ClosestSumOfThree.java

--- ClosestSumOfThree.java
+++ ClosestSumOfThree.java
@@ -1,11 +1,12 @@
-/**
+/** 
+ * 16_3Sum_Closest
  * Given an integer array nums of length n and an integer target
  * find three integers in nums such that the sum is closest to target.
  * Return the sum of the three integers.
  * You may assume that each input would have exactly one solution.
  */
 
-class Solution {
+class ClosestSumOfThree {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
         int j, k;
