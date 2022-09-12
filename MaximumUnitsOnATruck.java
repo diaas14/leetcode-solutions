15a6830689a5952015ca107b1ec93e8a4798b106
Diya S <60971672+diaas14@users.noreply.github.com>
2022-09-12 15:56:41 +0530

    Update and rename 1710_Maximum_Units_on_a_Truck.java to MaximumUnitsOnATruck.java

--- MaximumUnitsOnATruck.java
+++ MaximumUnitsOnATruck.java
@@ -1,4 +1,5 @@
 /**
+ * 1710_Maximum_Units_on_a_Truck
  * You are assigned to put some amount of boxes onto one truck. 
  * You are given a 2D array boxTypes, 
  * where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]: numberOfBoxesi is the number of boxes of type i.
@@ -8,7 +9,7 @@
  * You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
  * Return the maximum total number of units that can be put on the truck.
  */
-class Solution {
+class MaximumUnitsOnATruck {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         Arrays.sort(boxTypes, (a, b)->Integer.compare(b[1], a[1]));
         int maxUnits = 0;
